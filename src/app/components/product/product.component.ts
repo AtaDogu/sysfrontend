21c12031ece94e41fc18e68eb61cbb713e01a7df
AtaDogu <88247572+AtaDogu@users.noreply.github.com>
2022-05-25 23:10:48 +0300

    25.05

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,8 +1,10 @@
+import { CartService } from './../../services/cart.service';
 import { ProductService } from './../../services/product.service';
 
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-product',
@@ -13,9 +15,12 @@ export class ProductComponent implements OnInit {
 
   products:Product[]=[];
   dataLoaded=false;
+  filterText="";
 
   constructor(private productService:ProductService,
-    private activatedRoute:ActivatedRoute) { }
+    private activatedRoute:ActivatedRoute,
+    private toastrService:ToastrService,
+    private cartService:CartService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -38,6 +43,13 @@ export class ProductComponent implements OnInit {
       this.products=response.data
       this.dataLoaded=true
     })
+  }
+  addToCart(product:Product){
+    if (product.unitsInStock>0) {
+      this.toastrService.success("Added to Cart",product.productName)
+      this.cartService.addToCart(product);
+    }else {this.toastrService.error("The product could not be added to the cart","Error")}
+
   }
 
 }
